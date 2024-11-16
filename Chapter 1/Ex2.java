d4f72028486269cb27591a38f4c66a47096385d0
rohanulislam <rohanulislam.ict@gmail.com>
2024-11-16 20:27:53 +0600

    Update Ex2.java

--- Chapter 1/Ex2.java
+++ Chapter 1/Ex2.java
@@ -1,7 +1,6 @@
 public class Ch1Ex2 {
  public static void main(String[] args){
- System.out.println("Programming is fun!");
- System.out.println("Fundamentals First");
- System.out.println("Problem Driven");
+    for(int i=1;i<=5;i++)
+ System.out.println("Welcome to Java!");
  }
 }
