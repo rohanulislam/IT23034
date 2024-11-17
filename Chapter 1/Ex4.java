26b47403eba84caad061b93fa0813a356dfd992d
rohanulislam <rohanulislam.ict@gmail.com>
2024-11-17 17:58:47 +0600

    Update Ex4.java

--- Chapter 1/Ex4.java
+++ Chapter 1/Ex4.java
@@ -1,5 +1,11 @@
 public class Ch1Ex4 {
-    public static main(String[] args) {
-        System.out.println("Welcome to Java);
+    public static void main(String[] args) {
+        // have to print a table  which will conatain
+        // the main,square and cubic values of numbers from 1 to 5
+        System.out.println("a   a^2   a^3");
+        for(int i=1;i<=4;i++)
+        {
+            System.out.println(i+"    "+i*i+"    "+i*i*i);
+        }
     }
 }
