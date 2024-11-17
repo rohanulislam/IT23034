4881344dd9a1122df6e16e54a6d522a5f0aaab99
rohanulislam <rohanulislam.ict@gmail.com>
2024-11-17 17:59:48 +0600

    Update Ex6.java

--- Chapter 1/Ex6.java
+++ Chapter 1/Ex6.java
@@ -1,6 +1,14 @@
 public class Ch1Ex6 {
   public static void main(String[] args) {
-  System.out.println("Celsius 35 is Fahrenheit degree ");
-  System.out.println((9 / 5) * 35 + 32);
+    //sum of numbers from 1 to 9
+    int sum=0;
+    for(int i=1;i<=9;i++)
+    {
+        sum+=i;
+        if(i<9)
+        System.out.print(i+"+");
+        else System.out.print(i+"=");
+    }
+    System.out.println(sum);
   }
  }
