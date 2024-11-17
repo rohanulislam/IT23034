5ae363496454307e62264593f8b37f58b4b6536b
rohanulislam <rohanulislam.ict@gmail.com>
2024-11-17 17:57:22 +0600

    Update Ex3.java

--- Chapter 1/Ex3.java
+++ Chapter 1/Ex3.java
@@ -1,6 +1,9 @@
-public class Ch1Ex3
-{
-public static void main(String[] args) {
-System.out.println((10.5 + 2 * 3) / (45 - 3.5));
-}
+public class Ch1Ex3 {
+    public static void main(String[] args) {
+        // have to print java in the given manner
+        System.out.println("     J    A    V     V   A");
+        System.out.println("     J   A A    V   V   A A");
+        System.out.println("J    J  AAAAA    V V   AAAAA");
+        System.out.println(" J J   A     A    V   A     A");
+    }
 }
