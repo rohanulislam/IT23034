aeeef415c5619fa4530321c818e6372ec3a27217
rohanulislam <rohanulislam.ict@gmail.com>
2024-11-27 14:56:44 +0600

    Create Question.java

---
+++ Quizgame/Question.java
@@ -0,0 +1,11 @@
+public class Question {
+    String question;
+    String options;
+    char correctOption;
+
+    public Question(String question, String options, char correctOption) {
+        this.question = question;
+        this.options = options;
+        this.correctOption = correctOption;
+    }
+}
