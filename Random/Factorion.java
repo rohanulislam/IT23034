63f70dc4834c7e5b909f91358b2fb4ec347b0c5d
rohanulislam <rohanulislam.ict@gmail.com>
2024-12-06 21:08:11 +0600

    Update Factorion.java

--- Random/Factorion.java
+++ Random/Factorion.java
@@ -1,26 +1,13 @@
 import java.util.Scanner;
 public class Factorion {
-    public static int factorial(int num){
-        if (num == 0 || num == 1)
-        {
-            return 1;
-        }
-        int fact = 1;
-        for (int i = 2; i <= num; i++)
-        {
-            fact *= i;
-        }
-        return fact;
-    }
-
+    
     public static boolean isFactorion(int n) {
         int k = n;
         int sum = 0;
 
-        while (k > 0)
-        {
+        while (k > 0) {
             int digit = k % 10;
-            sum += factorial(digit);
+            sum += factorial.factorial(digit);
             k /= 10;
         }
         return sum == n;
@@ -33,10 +20,8 @@ public class Factorion {
         System.out.print("Enter the upper bound of the range: ");
         int upb = scan.nextInt();
         System.out.println("Factorion numbers in the range:");
-        for (int i = lb ;i <= upb; i++)
-        {
-            if (isFactorion(i))
-            {
+        for (int i = lb ;i <= upb; i++) {
+            if (isFactorion(i)) {
                 System.out.println(i);
             }
         }
